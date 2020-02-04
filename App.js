9466124b91c7830820d4a897fa06f06a4704eaa1
qwrwed <razk@hotmail.co.uk>
2020-02-04 12:45:16 +0000

    set up ESLint

--- App.js
+++ App.js
@@ -1,50 +1,36 @@
-import React, { Component } from "react";
-import RoutesAppContainer from "./Routes";
+import React, { Component } from 'react';
+import RoutesAppContainer from './Routes';
 import * as Font from 'expo-font';
 import { StatusBar, View, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = { assetsLoaded: false };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { assetsLoaded: false }
-    }
+  componentDidMount () {
+    Font.loadAsync({
+      'Geogtq-Bd': require('./src/assets/fonts/Ge2003-Bd.ttf'),
+      'Geogtq-Md': require('./src/assets/fonts/Ge2003-Md.ttf'),
+      'Geogtq-SmBd': require('./src/assets/fonts/Ge2003-SmBd.ttf')
+    }).then(() => {
+      this.setState({ assetsLoaded: true });
+    });
+  }
 
-    componentDidMount() {
-        Font.loadAsync({
-            'Geogtq-Bd': require('./src/assets/fonts/Ge2003-Bd.ttf'),
-            'Geogtq-Md': require('./src/assets/fonts/Ge2003-Md.ttf'),
-            'Geogtq-SmBd': require('./src/assets/fonts/Ge2003-SmBd.ttf'),
-        }).then(() => {
-            this.setState({ assetsLoaded: true });
-        })
+  render () {
+    const { assetsLoaded } = this.state;
+    if (assetsLoaded) {
+      return ( // only return app container if assets are loaded
+        <RoutesAppContainer />
+      );
+    } else {
+      return ( // otherwise, return activity indicator
+        <View style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
+          <ActivityIndicator />
+        </View>
+      );
     }
-
-    render() {
-        const { assetsLoaded } = this.state;
-        if (assetsLoaded) {
-            return ( // only return app container if assets are loaded
-                <RoutesAppContainer />
-            )
-        } else {
-            return ( // otherwise, return activity indicator
-                <View style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
-                    <ActivityIndicator />
-                </View>
-            )
-        }
-    }
+  }
 }
-
-
-
-/*
-
-
-    async componentDidMount() {
-        await Font.loadAsync({
-            'Ge2003-Bd': Ge2003_Bd,
-        });
-
-        this.setState({ assetsLoaded: true });
-    }*/
--- Routes.js
+++ Routes.js
@@ -1,18 +1,18 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-//all screens must be imported here:
-import Home from "./src/screens/HomeScreen";
-import Login from "./src/screens/LoginScreen";
-import Settings from "./src/screens/SettingsScreen";
-import Sections from "./src/screens/SectionsScreen";
-import Files from "./src/screens/FilesScreen";
-import Forum from "./src/screens/ForumScreen";
-import TopicView from "./src/screens/TopicViewScreen";
-import TopicCreate from "./src/screens/TopicCreateScreen";
+// all screens must be imported here:
+import Home from './src/screens/HomeScreen';
+import Login from './src/screens/LoginScreen';
+import Settings from './src/screens/SettingsScreen';
+import Sections from './src/screens/SectionsScreen';
+import Files from './src/screens/FilesScreen';
+import Forum from './src/screens/ForumScreen';
+import TopicView from './src/screens/TopicViewScreen';
+import TopicCreate from './src/screens/TopicCreateScreen';
 
-//test screen:
-//import ButtonListTest from "./src/screens/ButtonListTestScreen";
+// test screen:
+// import ButtonListTest from "./src/screens/ButtonListTestScreen";
 
 const AppNavigator = createStackNavigator(
   {
@@ -23,9 +23,9 @@ const AppNavigator = createStackNavigator(
     Forum,
     TopicView,
     TopicCreate,
-    Settings,
+    Settings
 
-    //ButtonListTest,
+    // ButtonListTest,
   },
   {
     initialRouteName: 'Home'
